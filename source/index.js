64b68767f04f8738b018146008648073a8a80f49
jaredianmills <jaredianmills@gmail.com>
2018-08-26 13:19:45 -0400

    fixed api urls

--- source/index.js
+++ source/index.js
@@ -48,8 +48,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  const usersUrl = "http://ancient-eyrie-51501.herokuapp.com/api/v1/users"
-  const scoresUrl = "http://ancient-eyrie-51501.herokuapp.com/api/v1/scores"
+  const usersUrl = "https://ancient-eyrie-51501.herokuapp.com/api/v1/users"
+  const scoresUrl = "https://ancient-eyrie-51501.herokuapp.com/api/v1/scores"
 
   userLogin.addEventListener("submit", event => {
     event.preventDefault()
